0f2752da838d74aea9d37177f6f2f9fa69fce785
Suwan <imissyou5466@gmail.com>
2024-03-27 00:05:44 +0900

    ✅ 3장 진행중

--- public/docs/3-Underscore.js를-직접-만들며-함수형-자바스크립트의-뼈대-익히기.md
+++ public/docs/3-Underscore.js를-직접-만들며-함수형-자바스크립트의-뼈대-익히기.md
@@ -63,3 +63,84 @@ console.log(_.contains(list, 3)); // true
 
 console.log(_.isArray(list)); // true
 ```
+
+**객체에 대한 Undescore.js 관점**
+
+```js
+/* 객체에 Values만 뽑은 새로운 배열을 Return */
+console.log(_.values({id: 1, name: "ID", age: 32})); // [ 1, 'ID', 32 ]
+
+/* 객체에 Keys만 뽑은 새로운 배열을 Return */
+console.log(_.keys({id: 1, name: "ID", age: 32}));
+
+/* 왼쪽에 있는 객체에 대해 오른쪽에 있는 객체를 덮여씌운다 */
+console.log(
+  _.extend(
+    {id: 1, name: "ID", age: 32},
+    {id: 1, name: "ID", age: 36, job: "developer"}
+  )
+); // { id: 1, name: 'ID', age: 36, job: 'developer' }
+
+/* 객체에 특정 key/value를 수집한다. 인자는 key */
+console.log(pick({id: 1, name: "ID", age: 32}, "id", "age")); // { id: 1, age: 32 }
+
+/* 두번째 인자로 넘겨진 Key들을 제외한 새로이 반환된 객체를 얻는다. */
+console.log(omit({id: 1, name: "ID", age: 32}, "id"));
+// { name: 'ID', age: 32 }
+```
+
+<br />
+
+### \_.negate
+
+- 해당 함수는 원래 함수의 결과를 반대로 바꾸는 **함수를** 리턴
+
+```js
+/* _.negate */
+const equal5 = (a) => {
+  return a === 5;
+};
+console.log(equal5(5)); // true
+/* _.negate 적용 후 */
+const negate5 = negate(equal5);
+console.log(negate5(5)); // false
+```
+
+**\_.negate 실제 구현부**
+
+```js
+_.negate = (func) => {
+  return function () {
+    return !func.apply(this, arguments); // 받아온 함수를 실행 후 !한다.
+  };
+};
+```
+
+<br />
+
+### Underscore 체이닝
+
+- 2번째에서 `._chain` 함수의 `value()`로 최종값을 얻어내었다.
+- 체인방식으로 코드 작성시 위에서 아래로 순차적으로 코드를 읽어나갈 수 있어 가독성이 증가한다.
+- `.value()`를 실행하기 전까지 메서드를 계속 체이닝할 수 있게된다.
+
+```js
+/* Underscore 체이닝 */
+
+/* 1. 함수형 방식 */
+const underscoreChain = filter(
+  map([1, 2, 3], (val) => val * 2),
+  (val) => val <= 4
+);
+
+console.log(underscoreChain); // [2,4]
+
+/* 2. Underscore _.chain 함수를 사용하는 방식 */
+const underScoreChain2 = chain([1, 2, 3])
+  .map((n) => n * 2)
+  .filter((n) => n <= 4)
+  .value();
+console.log(underScoreChain2); // [2,4]
+```
+
+<br />
--- src/3-Underscore.js를-직접-만들며-함수형-자바스크립트의-뼈대-익히기/index.js
+++ src/3-Underscore.js를-직접-만들며-함수형-자바스크립트의-뼈대-익히기/index.js
@@ -1,4 +1,5 @@
-import _, {each} from "underscore";
+import _, {chain, each, filter, map, negate, omit, pick} from "underscore";
+import lodash from "lodash";
 
 /* 3.1.1 Undersocr.js 간단히 써보기 */
 console.log(
@@ -33,7 +34,7 @@ const USERS = [
   {id: 8, name: "알폰소 데이비스", age: 21},
 ];
 
-console.log(_.pluck(USERS, "name")); // [손흥민, 리오넬 메시, 크리스티아누 호날두 등등..]
+/* console.log(_.pluck(USERS, "name")); // [손흥민, 리오넬 메시, 크리스티아누 호날두 등등..]
 
 console.log(_.first(USERS));
 
@@ -46,4 +47,67 @@ console.log(_.last(USERS), 2);
 console.log(_.rest(USERS), "rest 함수");
 
 console.log(_.initial(USERS));
-console.log(_.initial(USERS), 2);
+console.log(_.initial(USERS), 2); */
+
+/* 객체에 대한 Undescore.js  */
+
+/* 객체에 Values만 뽑은 새로운 배열을 Return */
+console.log(_.values({id: 1, name: "ID", age: 32})); // [ 1, 'ID', 32 ]
+
+/* 객체에 Keys만 뽑은 새로운 배열을 Return */
+console.log(_.keys({id: 1, name: "ID", age: 32}));
+
+/* 왼쪽에 있는 객체에 대해 오른쪽에 있는 객체를 덮여씌운다 */
+console.log(
+  _.extend(
+    {id: 1, name: "ID", age: 32},
+    {id: 1, name: "ID", age: 36, job: "developer"}
+  )
+); // { id: 1, name: 'ID', age: 36, job: 'developer' }
+
+/* 객체에 특정 key/value를 수집한다. 인자는 key */
+console.log(pick({id: 1, name: "ID", age: 32}, "id", "age")); // { id: 1, age: 32 }
+
+/* 두번째 인자로 넘겨진 Key들을 제외한 새로이 반환된 객체를 얻는다. */
+console.log(omit({id: 1, name: "ID", age: 32}, "id"));
+// { name: 'ID', age: 32 }
+
+/* _.negate */
+const equal5 = (a) => {
+  return a === 5;
+};
+console.log(equal5(5)); // true
+/* _.negate 적용 후 */
+const negate5 = negate(equal5);
+console.log(negate5(5)); // false
+
+/* Underscore 체이닝 */
+
+const underscoreChain = filter(
+  map([1, 2, 3], (val) => val * 2),
+  (val) => val <= 4
+);
+
+console.log(underscoreChain); // [2,4]
+
+/* Underscore _.chain 함수를 사용하는 방식 */
+const underScoreChain2 = chain([1, 2, 3])
+  .map((n) => n * 2)
+  .filter((n) => n <= 4)
+  .value();
+console.log(underScoreChain2); // [2,4]
+
+/* take를 통한 지연평가 예시 */
+
+const MOCK_LIST = _.range(50); // 1부터 49까지의 배열 생성
+
+let lodash_i = 0;
+const results2 = lodash
+  .chain(MOCK_LIST)
+  .filter((num) => {
+    lodash_i++;
+    return num % 2 === 0;
+  })
+  .take(5)
+  .value();
+console.log(results2);
